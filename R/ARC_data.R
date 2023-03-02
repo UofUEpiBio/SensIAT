0e517acbcf604fafb31a68f1a86fdc90facd0bda
Andrew Redd <andrew.redd@hsc.utah.edu>
2023-03-02 12:33:18 -0700

    Added ARC_data

---
+++ R/ARC_data.R
@@ -0,0 +1,16 @@
+#' ARC Example Data
+#'
+#' TODO: Description
+#'
+#' @format ## `ARC_data`
+#' A data frame with 1,231 rows and 5 columns one row per visit for 301 unique subjects.
+#' \describe{
+#'  \item{elig_pid}{Patient identifier.  301 unique values.}
+#'  \item{Asthma_control}{TODO}
+#'  \item{time}{Day of visit from baseline, 0 to 1001.}
+#'  \item{Trt}{Treatment/Control group. 625 `control` and 606 treatment(`home_visits`).}
+#'  \item{visit_number}{visit number within patient, from 0 to 4}
+#' }
+#'
+#' @source TODO
+"ARC_data"
