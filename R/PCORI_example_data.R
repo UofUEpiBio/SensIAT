a4c87c3c5b9965bd5ce5452f61c63ccfe17ac3ce
Andrew Redd <u0092104@ad.utah.edu>
2024-07-02 15:18:15 -0600

    Added PCORI_example_data.

---
+++ R/PCORI_example_data.R
@@ -0,0 +1,14 @@
+#' PCORI Example Data
+#'
+#' A simulated dataset for use in the PCORI tutorial, testing and documentation.
+#'
+#' @format A data frame with 779 rows and 4 variables consisting of 200
+#' simulated patients.  Each row in the data represents a visit for the patient.
+#' The columns are:
+#' \describe{
+#'      \item{Subject_id}{A unique identifier for each patient.}
+#'      \item{Visit}{The ordinal number of the visit for the patient.  Baseline observation is 0.}
+#'      \item{Time}{The time of the visit in days, since baseline.}
+#'      \item{Outcome}{The outcome of interest.}
+#' }
+"PCORI_example_data"
